fec0336e6ba5f3a369ad75ee3955b3d84150a9b2
Manoj <96427652+M-an-o-j@users.noreply.github.com>
2023-09-27 11:29:14 +0530

    Update Alienemail.js

--- email_server/controllers/Alienemail.js
+++ email_server/controllers/Alienemail.js
@@ -24,38 +24,5 @@ const AlienEmail = async (req, res, next) => {
 
 }
 
-// const OtpEmail = async (req, res, next) => {
-
-//     const { email } = req.body
-//     function getRandom(max) {
-//         return Math.round(Math.random() * max)
-//     }
-
-//     for (let step = 0; step < 5; step++) {
-
-//         otp += getRandom(6)
-//     }
-
-//     const message = `Hey this is ${otp}`
-
-//     try {
-//         sendOtp({
-//             email: email,
-//             subject: "message from aliens",
-//             message
-//         })
-//     } catch (error) {
-//         res.status(400).json({
-//             message: error
-//         })
-//     }
-
-//     res.status(200).json({
-//         message: `send it successfully !!`,
-//         otp
-//     })
-
-//     otp = ""
-// }
 
 module.exports = { AlienEmail }
